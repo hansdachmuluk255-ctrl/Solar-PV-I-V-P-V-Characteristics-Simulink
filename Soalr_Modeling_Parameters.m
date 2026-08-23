a57c26c31aec6ca1428e4e3e1656ad9b815c0358
Muluk Chand Hansda <hansdachmuluk255@gmail.com>
2026-08-23 07:57:14 +0530

    Add files via upload

---
+++ Soalr_Modeling_Parameters.m
@@ -0,0 +1,13 @@
+% Solar PV Model Parameters
+
+ki = 0.0032 ;      % short circuit current of the cell at 
+q = 1.602e-19;     % electron charge
+k = 1.380648e-23;  % boltzman constant 
+n = 1.29;          % ideality factor of diode 
+Ego = 1.1;         % band gap energy of the semiconductor 
+Rs = 0.1896;       % Series resistance (ohms)
+Rsh = 106.18;      % Shunt resistance (ohms)
+Tn = 298;          % Nominal Temperature (kelvin)
+Voc = 36.06;       % Open circuit voltage (Volt)
+Isc = 7.95;        % short Circuit voltage (Volt)
+Ns = 60;           % Total No. of cells connected in series
